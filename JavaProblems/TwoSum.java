b38c4b47435ca4cb979bb5d224f2114e1c675e99
toghrulabdullayev <toghrulabdullayev1@gmail.com>
2026-02-18 17:18:11 +0400

    add TwoSum solution

--- JavaProblems/TwoSum.java
+++ JavaProblems/TwoSum.java
@@ -1,11 +1,25 @@
-class Solution {
-    public int[] twoSum(int[] nums, int target) {
+// returns indices in an array
+public int[] twoSum(int[] nums, int target) {
+    Map<Integer, Integer> numMap = new HashMap<>();
 
+    // converting the array into a map: O(n)
+    for (int i = 0; i < nums.length; i++) {
+        // suppose target = 9. If the element is 2, the 2nd should be 7
+        int diff = target - nums[i];
+        // looking up for the second value: O(1)-O(n)
+        if (numMap.containsKey(diff)) {
+            return new int[]{numMap.get(diff), i};
+        }
+        // key=num -> value=index: O(1)-O(n)
+        numMap.put(nums[i], i);
     }
+
+    return null;
 }
 
-public class TwoSum {
-    static void main() {
-        var solution = new Solution();
-    }
+// Overall complexity (must be < O(n^2)): O(n)
+void main() {
+    System.out.println(Arrays.toString(twoSum(new int[]{2, 7, 11, 15}, 9)));
+    System.out.println(Arrays.toString(twoSum(new int[]{3, 2, 4}, 6)));
+    System.out.println(Arrays.toString(twoSum(new int[]{3, 3}, 6)));
 }
